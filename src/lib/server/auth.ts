a14a995efc4f2964f8e6ac1242cc9c7318559d1c
tugnodev <tugnodev@gmail.com>
2025-09-07 17:19:56 +0000

    debug

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -10,19 +10,30 @@ const { PrismaClient } = pkg;
 const prisma = new PrismaClient();
 export const auth = betterAuth({
     appName: "Binabusiness",
+    secret: env.BETTER_AUTH_SECRET!,
+    baseURL: env.ENV === "prod" ? "https://binabusiness.vercel.app" : "http://localhost:5173",
+    basePath: "api/auth",
+    origin: env.ENV === "prod" ? "https://binabusiness.vercel.app" : "http://localhost:5173",
     trustedOrigins: [
         "http://localhost:5173",
         "http://192.168.1.16:5173",
         "https://binabusiness.vercel.app",
     ],
+    cookies: {
+        secure: true,
+        sameSite: "strict",
+        httpOnly: true
+    },
     database: prismaAdapter(
         prisma, 
-        { provider: "sqlite" }),
+        { provider: "sqlite" }
+    ),
     socialProviders: {
         google: {
             prompt: "select_account",
             clientId: env.GOOGLE_ID!,
             clientSecret: env.GOOGLE_SECRET!,
+            callbackURL: env.ENV === "prod" ? "https://binabusiness.vercel.app/api/auth/callback/google" : "http://localhost:5173/api/auth/callback/google",
         },
     },
     emailAndPassword: {
